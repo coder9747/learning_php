ec9a87615e2f503b1f90b4b38c464923728c366c
Your Name <pratyushkarn007@gmail.com>
2024-04-27 21:03:24 +0530

    added signup funtly

--- project_1/database/connect.php
+++ project_1/database/connect.php
@@ -5,6 +5,5 @@ define("USER", "root");
 define("PASSWORD", "");
 define("DATABASE_NAME", "note");
 
+//connecting to database
 $conn = mysqli_connect(HOST_NAME, USER, PASSWORD, DATABASE_NAME) or die(mysqli_error($conn));
-
-echo "connected Succesful";
